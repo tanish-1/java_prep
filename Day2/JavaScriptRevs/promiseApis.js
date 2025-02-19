a2c03acbffe094e61ebf6a05303462fcabb33f79
Tanish <imtanishlamba@gmail.com>
2025-02-19 17:48:36 +0530

    changes are again made for day2

--- Day2/JavaScriptRevs/promiseApis.js
+++ Day2/JavaScriptRevs/promiseApis.js
@@ -1,2 +1,49 @@
+//promise APIs 
 // promise.all 
 // promise.allSetteled
+// promise.race
+// promise.any
+
+
+const p1 = new Promise((resolve, reject)=>{
+    setTimeout(()=> reject("P1 reject"), 3000);
+}) 
+const p2 = new Promise((resolve, reject)=>{
+    setTimeout(()=> reject("P2 reject"), 4000);
+}) 
+const p3 = new Promise((resolve, reject)=>{
+    setTimeout(()=> reject("P3 fail"), 2000);
+})  
+
+// a) promise.all
+
+// Promise.all([p1,p2 ,p3])
+// .then(function(result){
+//     console.log(result)
+// })
+// .catch((err)=>console.log(err))
+
+// b) promise.allSetteled
+
+// Promise.allSettled([p1,p2 ,p3])
+// .then(function(result){
+//     console.log(result)
+// })
+// .catch((err)=>console.log(err))
+
+// c) promise.race
+
+// Promise.race([p1,p2 ,p3])
+// .then(function(result){
+//     console.log(result)
+// })
+// .catch((err)=>console.log(err))
+
+// d) promise.any
+Promise.any([p1,p2 ,p3])
+.then(function(result){
+    console.log(result)
+})
+.catch((err)=>{ 
+  console.log(err.errors)
+})
