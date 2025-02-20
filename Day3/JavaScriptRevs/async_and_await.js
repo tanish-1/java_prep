a2586dca7093cbd4fb528c3e6a7828d475612ff2
Tanish <imtanishlamba@gmail.com>
2025-02-20 19:06:36 +0530

    changes are made for Day3

---
+++ Day3/JavaScriptRevs/async_and_await.js
@@ -0,0 +1,58 @@
+const API_URL  = "https://api.github.co/";
+async function handlePromise(){
+            try{
+          const response = await fetch(API_URL);
+        const data = await response.json();
+        console.log(data); 
+            }catch(err){
+                console.log(err);
+            }
+}
+
+handlePromise()
+// const promise1 = new Promise((resolve, reject)=> {
+//     setTimeout(()=> {
+//         resolve("Promise Resolved value!!");
+//     }, 5000);
+// })
+
+// const promise2 = new Promise((resolve, reject)=> {
+//     setTimeout(()=> {
+//         resolve("Promise Resolved value!!");
+//     },10000);
+// })
+
+
+// // function getdata() {
+// promise.then((value)=> console.log(value))
+// console.log("nameste javascript");
+// }
+// getdata();
+// async function handlePromise() {
+//     console.log("ahhh")
+//      const value1 = await promise1;
+//      console.log("nameste javascript 1")
+//      console.log(value1);
+
+//      console.log("yess yess")
+
+//      const value2 = await promise2;
+//      console.log("nameste javascript 2")
+//      console.log(value2);
+// }
+// handlePromise();
+// console.log("uhhh")
+// function getdata() {
+//     promise.then((res)=> console.log(res))
+// }
+// getdata()
+// async function getdata(){
+//      return promise
+// }
+// const data = getdata();
+// console.log(data);
+// data
+// .then((res)=>console.log(res))
+// .catch((err)=>console.log(err))
+
+
