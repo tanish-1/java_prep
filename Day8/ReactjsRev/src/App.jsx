a48b5ab335628d476a60d057827033de44edfc9f
Tanish <imtanishlamba@gmail.com>
2025-03-01 04:36:58 +0530

    changes are applied

--- Day8/ReactjsRev/src/App.jsx
+++ Day8/ReactjsRev/src/App.jsx
@@ -1,29 +1,17 @@
-import { useState } from 'react';
+
 import './App.css'
+import AnotherHero from './component/AnotherHero'
+// import HeroSection from './component/HeroSection'
 
 function App() {
 
-let [counter,setCounter] = useState(0)
-
-const addValue = () => {
-  if(counter === 20) return;
-  setCounter(counter + 1);
-  console.log(counter);
-};
-
-const removeValue = () => {
-  if (counter<=0) return;
-  setCounter(counter - 1);
-}
 
   return (
     <>
-     <h1>chai aur react</h1>
-     <h1>counter value : {counter}</h1>
-     
-     <button onClick={addValue}>Add Value</button>
-     <br />
-     <button onClick={removeValue}>remove value</button>
+     {/* <h1 className='bg-green-400 p-4'>chai aur react</h1> */}
+      <AnotherHero channel="Chai Aur Code" myName = {{name:"tanish"}}/>
+      {/* <HeroSection/> */}
+      <AnotherHero channel="hiii" myName = {{}}/>
     </>
   )
 }
